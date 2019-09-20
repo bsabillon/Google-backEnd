2e352b901c31286dcd73e0d28f1b69a85aa113a1
bsabillon <38368514+bsabillon@users.noreply.github.com>
2019-09-20 00:31:34 -0600

    initial commit

---
+++ routes/books.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const appServices = require('../Google-backEnd/applicationServices/appServices');
+const db = require('../../config/database');
+const bodyParser = require('body-parser');
+const Pokemon = require('../models/pokemon');
+
+
+// router.post('/add',appServices.addBook);
+// router.get('/',appServices.getBooks);
+// router.get('/byId/:id?',appServices.getBookById);
+// router.put('/update/:id?',appServices.updateBook);
+// router.delete('/delete/:id?',appServices.deleteBook);
+
+
+module.exports = router;
